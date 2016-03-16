f799f8a534da2c8b469f01a0c395883ddb81661e
Matt Hosack <hosack.matt@gmail.com>
2016-03-16 14:40:33 -0700

    Made simulate a recursive function.  Removed printing code to use logging module.  Removed unused code.

--- compare.py
+++ compare.py
@@ -1,50 +1,74 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import stats
 import scraper
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger("march.maddness.log")
+logger.setLevel(logging.DEBUG)
 
 
-def compare(h, v):
-    """Compare home team stats vs visitor team stats
-    """
-    hs = scraper.get_stats(h)
-    vs = scraper.get_stats(v)
-
-    # local references to functions
-    gos = stats.get_overall_score
-    gsh = stats.get_shooting
-    gto = stats.get_turnovers
-    grb = stats.get_rebounds
-    gft = stats.get_freethrows
-
-    # print(out some info for the user.  update this to use logging module)
-    print("******** {} ********\t******** {} ********".format(h, v))
-    print("OVERALL : {}\tOVERALL : {}".format(gos(hs), gos(vs)))
-    print("shooting: {}\tshooting: {}".format(gsh(hs), gsh(vs)))
-    print("turnover: {}\tturnover: {}".format(gto(hs), gto(vs)))
-    print("rebounds: {}\trebounds: {}".format(grb(hs), grb(vs)))
-    print("freethro: {}\tfreethro: {}\n\n".format(gft(hs), gft(vs)))
-
+def compare(home, visitor):
+    "Compare home team stats vs visitor team stats"
     # get overall home and visitor stats
-    ho = gos(hs)
-    vo = gos(vs)
+    homestats = scraper.get_stats(home)
+    visitorstats = scraper.get_stats(visitor)
+    homeoverall = stats.get_overall_score(homestats)
+    visitoroverall = stats.get_overall_score(visitorstats)
 
     # finally do the comparison
-    if ho > vo:
-        print("WINNER = {} by {}".format(h, (ho-vo)*100))
-        return h
-    elif ho < vo:
-        print("WINNER = {} by {}".format(v, (vo-ho)*100))
-        return v
-    else:
-        print("TIE?!?!!!???!!!!!!!")
-        return h
+    return home if homeoverall >= visitoroverall else visitor
 
+
+def simulate(region, teams):
+    """
+    Takes a list of team names and delivers the winner after comparing each
+    pair of teams two at a time.
+    For example, a bracket of ["team1", "team2", "team3", "team4"] would
+    be simulated as:
+        compare("team1", "team2") => winner "team2"
+        compare("team3", "team4") => winner "team3"
+
+        teams_left = ["team2", "team3"]
+        compare("team2", "team3") => winner "team3"
+    """
+    if len(teams) < 2:
+        return teams[0]
+
+    rnd = 16 // len(teams)
+    logger.info("{} Round {}: {}".format(region, rnd, teams))
+
+    # zip matchups from what's left in the teams list
+    # [1, 2, 3, 4] => [(1, 2), (3, 4)]
+    matchups = list(zip(teams[::2], teams[1::2]))
+    # empty teams so that we can replace it with the winners
+    teams = []
+
+    # simulate each matchup and put the winner back into teams list
+    for match in matchups:
+        home, visitor = match
+        winner = compare(home, visitor)
+
+        teams.append(winner)
+
+        logger.info("simulating: {} vs. {} ==> {}".format(
+            home,
+            visitor,
+            winner))
+
+    return simulate(region, teams)
 
 if __name__ == "__main__":
-    with open("matchups.txt", "r") as f:
-        for line in f:
-            home, visitor = line.split()
-            try:
-                compare(home, visitor)
-            except Exception as e:
-                print("Exception thrown", e)
+    import yaml
+    matchups = yaml.load(open("matchups.yml"))
+    winners = {}
+
+    for region, teams in matchups.items():
+        winners[region] = simulate(region, teams)
+
+    # create new bracket of 4 regional champions
+    finalfour = [winners["west"], winners["west"],
+                 winners["midwest"], winners["east"]]
+
+    # simulate final four bracket
+    champ = simulate("finalfour", finalfour)
