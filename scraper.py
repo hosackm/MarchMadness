69476405d7de6b554308867c3e9477daa5504603
Matt Hosack <hosack.matt@gmail.com>
2016-03-16 15:55:41 -0700

    Adding caching of statistics so that we don't have to requests data that hasn't changed recently

--- scraper.py
+++ scraper.py
@@ -1,7 +1,35 @@
 from __future__ import print_function
+from bs4 import BeautifulSoup as Soup
+import pickle
 import requests
 import teams
-from bs4 import BeautifulSoup as Soup
+import time
+import os
+
+
+def load_cache():
+    if os.path.exists(CACHEFILENAME):
+        with open(CACHEFILENAME, "rb") as f:
+            return pickle.load(f)
+    else:
+        return {"lastupdate": time.time()}
+
+CACHEFILENAME = "marchmadness.pickle"
+CACHE = load_cache()
+
+
+def get_from_cache(team):
+    lastupdate = CACHE.get("lastupdate", 0)
+    if time.time() - lastupdate > 86400:  # more than a day old
+        return None
+    return CACHE.get(team)
+
+
+def add_to_cache(team, teamstats):
+    CACHE.update({"lastupdate": time.time()})
+    CACHE.update({team: teamstats})
+    with open(CACHEFILENAME, "wb") as f:
+        f.write(pickle.dumps(CACHE))
 
 
 def get_team_ext(team):
@@ -14,6 +42,10 @@ def get_stats(team):
     stats from espn.com.
     Return: dictionary containing all stats
     """
+    cachetry = get_from_cache(team)
+    if cachetry is not None:
+        return cachetry
+
     url_base = "http://espn.go.com/mens-college-basketball/team/stats/_/id"
     team_ext = get_team_ext(team)
     if not team_ext:
@@ -35,30 +67,35 @@ def get_stats(team):
     (_, _, FGM, FGA, FTM, FTA,
         TPM, TPA, PTS, OFFR, DEFR, REB, AST, TO, STL, BLK) = season
 
-    return dict(gp=float(GP),
-                ppg=float(PPG),
-                rpg=float(RPG),
-                apg=float(APG),
-                spg=float(SPG),
-                bpg=float(BPG),
-                tpg=float(TPG),
-                fgp=float(FGP),
-                ftp=float(FTP),
-                tpp=float(TPP),
-                fgm=float(FGM),
-                fga=float(FGA),
-                ftm=float(FTM),
-                fta=float(FTA),
-                tpm=float(TPM),
-                tpa=float(TPA),
-                pts=float(PTS),
-                offr=float(OFFR),
-                defr=float(DEFR),
-                reb=float(REB),
-                ast=float(AST),
-                to=float(TO),
-                stl=float(STL),
-                blk=float(BLK))
+    teamstats = dict(gp=float(GP),
+                     ppg=float(PPG),
+                     rpg=float(RPG),
+                     apg=float(APG),
+                     spg=float(SPG),
+                     bpg=float(BPG),
+                     tpg=float(TPG),
+                     fgp=float(FGP),
+                     ftp=float(FTP),
+                     tpp=float(TPP),
+                     fgm=float(FGM),
+                     fga=float(FGA),
+                     ftm=float(FTM),
+                     fta=float(FTA),
+                     tpm=float(TPM),
+                     tpa=float(TPA),
+                     pts=float(PTS),
+                     offr=float(OFFR),
+                     defr=float(DEFR),
+                     reb=float(REB),
+                     ast=float(AST),
+                     to=float(TO),
+                     stl=float(STL),
+                     blk=float(BLK))
+
+    # save to cache
+    add_to_cache(team, teamstats)
+
+    return teamstats
 
 
 def list_teams():
