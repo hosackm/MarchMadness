b221199255a1ee29b7e122b36865cfc0dc801b8e
Matt Hosack <hosack.matt@gmail.com>
2016-03-16 10:59:54 -0700

    fixing return None bug

--- compare.py
+++ compare.py
@@ -1,5 +1,6 @@
 import sys
 import stats
+import teams
 import scraper
 
 
@@ -27,10 +28,32 @@ def compare(h, v):
 
     if ho > vo:
         print "WINNER = {} by {}".format(h, (ho-vo)*100)
+        return h
     elif ho < vo:
         print "WINNER = {} by {}".format(v, (vo-ho)*100)
+        return v
     else:
         print "TIE?!?!!!???!!!!!!!"
+        return h
+
+
+def sim_bracket():
+    """Run through matchups and simulate the bracket while printing
+    out the results.  This may be rewritten as a recursive function
+    """
+    matchups = teams.matchups
+    while len(matchups):
+        winners = []
+        for matchup in matchups:
+            print "Matchup: {} vs {}".format(matchup[0], matchup[1])
+            winner = compare(matchup[0], matchup[1])
+            winners.append(winner)
+            print "Winner: {}".format(winner)
+        if len(winners) == 1:
+            return winners[0]
+        print "winners: {}".format(winners)
+        matchups = zip(winners)
+        print "zipped to: {}".format(matchups)
 
 
 def usage(filename):
